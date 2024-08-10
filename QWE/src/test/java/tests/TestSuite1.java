6f12a6f5baaba7d62906fe0ec891b3af6e2aca40
kishore <skill2automate@gmail.com>
2024-08-10 09:59:52 +0530

    commit2

--- QWE/src/test/java/tests/TestSuite1.java
+++ QWE/src/test/java/tests/TestSuite1.java
@@ -3,10 +3,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class TestSuite1 {
 
 	@Test
 	public void test1() {
+		String path="C:\\Users\\suman\\eclipse-workspace_3\\QWE\\target\\jenkinsreport.html";
+		ExtentReports report= new ExtentReports(path);
+		ExtentTest test= report.startTest("test case01");
+		test.log(LogStatus.PASS, "test started");
+		test.log(LogStatus.INFO, "test  execution in in progress");
+		test.log(LogStatus.PASS, "test closed");
+		report.endTest(test);
+		report.flush();
+		report.close();
 		System.out.println("test1");
 	}
 	@Test
